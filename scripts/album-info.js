ccfa4d30543e1ebe3a317cdbe42f46bef4735411
Jordan Williams <jordanmswilliams@gmail.com>
2019-04-17 15:13:25 -0500

    assignment-intro-albuminfo:completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,7 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  // Use '.' in front of class and '#' in front of id //
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
 }
