0e7c3961f4d44279b06dff6dde81ee45fc6e0119
Jordan Williams <jordanmswilliams@gmail.com>
2019-04-18 15:01:28 -0500

    assignment-player-bar-buttons:completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,17 +1,31 @@
 {
   $('button#play-pause').on('click', function() {
-     player.playPause();
-     $(this).attr('playState', player.playState);
-   });
+  player.playPause();
+  $(this).attr('playState', player.playState);
+  });
 
-   $('button#next').on('click', function() {
-     if (player.playState !== 'playing') { return; }
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; }
 
-     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex + 1;
-     if (nextSongIndex >= album.songs.length) { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) { return; }
 
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
-   });
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+
+  });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex - 1;
+    if (nextSongIndex >= album.songs.length) { return; }
+
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+
+  });
+
 }
